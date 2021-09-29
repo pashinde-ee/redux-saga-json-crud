47260b0361e8c03cf5b5470f265eefc36377d2fc
Prashant <pr.shinde@easternenterprise.com>
2021-09-29 19:07:05 +0530

    added users list

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,21 @@
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import createSagaMiddleware from "@redux-saga/core"
 import logger from "redux-logger"
 import rootReducer from "./rootReducer"
-// import rootSaga from "./usersagas"
+import rootSaga from "./usersagas"
 
 const sagaMiddleware = createSagaMiddleware()
 
 const middlewares = [sagaMiddleware]
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)))
 
-//sagaMiddleware.run()
+sagaMiddleware.run(rootSaga)
 
 export default store;
